40628b6c6821aec4d1b8fa205b3420e15619ddd9
inamuu <8310973+inamuu@users.noreply.github.com>
2022-02-08 19:44:53 +0900

    Change params

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-e@vfwav1+m4q)dl$v#s1l1xtvkj+y3m-rf*hhn8vx0oac7#151
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 # Application definition
 
@@ -116,8 +115,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
